17eecabe7636cd3a95c14a08da01faba11eff793
GustavoEduardo <gustavolimaeduardo@gmail.com>
2023-02-21 13:55:50 -0300

    icones e splash cordova

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.vofazer.app',
-  appName: 'vofazer',
+  appName: 'A Fazer',
   webDir: 'dist/vofazer',
   bundledWebRuntime: false
 };
